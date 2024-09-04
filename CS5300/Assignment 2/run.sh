09ce718ecb2b3b39adb1bbd108c863cfaef2daba
TushitaSharva <tushitasharva03@gmail.com>
2024-09-04 17:09:35 +0530

    Final Commit

--- CS5300/Assignment 2/run.sh
+++ CS5300/Assignment 2/run.sh
@@ -6,14 +6,14 @@ if [ -n "$(ls -A "$outputs_directory")" ]; then
 fi
 
 # To run for a particular file
-# input_file_name="inp.txt"
-# g++ -std=c++14 Assgn2-Chunk-CS21BTECH11022.cpp -fopenmp -lm && ./a.out "$input_file_name"
-# g++ -std=c++14 Assgn2-Mixed-CS21BTECH11022.cpp -fopenmp -lm && ./a.out "$input_file_name"
-# g++ -std=c++14 Assgn2-Dynamic-CS21BTECH11022.cpp -fopenmp -lm && ./a.out "$input_file_name"
+input_file_name="inp.txt"
+g++ -std=c++14 Assgn2-Chunk-CS21BTECH11022.cpp -fopenmp -lm && ./a.out "$input_file_name"
+g++ -std=c++14 Assgn2-Mixed-CS21BTECH11022.cpp -fopenmp -lm && ./a.out "$input_file_name"
+g++ -std=c++14 Assgn2-Dynamic-CS21BTECH11022.cpp -fopenmp -lm && ./a.out "$input_file_name"
 
 # To loop through all files in the subfolders of the 'testcases' directory and run all the test cases
-for input_file_name in ./testcases/*/*.txt; do
-    g++ -std=c++14 Assgn2-Chunk-CS21BTECH11022.cpp -fopenmp -lm && ./a.out "$input_file_name"
-    g++ -std=c++14 Assgn2-Mixed-CS21BTECH11022.cpp -fopenmp -lm && ./a.out "$input_file_name"
-    g++ -std=c++14 Assgn2-Dynamic-CS21BTECH11022.cpp -fopenmp -lm && ./a.out "$input_file_name"
-done
+# for input_file_name in ./testcases/*/*.txt; do
+#     g++ -std=c++14 Assgn2-Chunk-CS21BTECH11022.cpp -fopenmp -lm && ./a.out "$input_file_name"
+#     g++ -std=c++14 Assgn2-Mixed-CS21BTECH11022.cpp -fopenmp -lm && ./a.out "$input_file_name"
+#     g++ -std=c++14 Assgn2-Dynamic-CS21BTECH11022.cpp -fopenmp -lm && ./a.out "$input_file_name"
+# done
