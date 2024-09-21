c2c20ac84eb266e9a188e2f45196fbb40fa10432
TushitaSharva <tushitasharva03@gmail.com>
2024-09-21 11:17:15 +0530

    Bash Script

---
+++ CS5300/Assignment 3/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+outputs_directory="./outputs"
+if [ -n "$(ls -A "$outputs_directory")" ]; then
+    rm "$outputs_directory"/*
+fi
+
+# To run for a particular file
+input_file_name="inp.txt"
+g++ -std=c++17 Bakery-CS21BTECH11022.cpp -fopenmp -lm && ./a.out "$input_file_name"
+
+# To loop through all files in the subfolders of the 'testcases' directory and run all the test cases
+# for input_file_name in ./testcases/*/*.txt; do
+#     g++ -std=c++17 Assgn2-Chunk-CS21BTECH11022.cpp -fopenmp -lm && ./a.out "$input_file_name"
+#     g++ -std=c++17 Assgn2-Mixed-CS21BTECH11022.cpp -fopenmp -lm && ./a.out "$input_file_name"
+#     g++ -std=c++17 Assgn2-Dynamic-CS21BTECH11022.cpp -fopenmp -lm && ./a.out "$input_file_name"
+# done
